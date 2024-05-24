d50739acc86ceba7f081d356e7ad3a58f8f34f13
mynameisjul <jublaye@hotmail.fr>
2024-05-24 18:22:48 +0200

    modified setup script so folder are not updated if they already exist

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ mkdir -p "cpp_module_$mod_number"
 cd cpp_module_$mod_number 
 for i in 0 1 2 3 4 5 6
 do
-	mkdir ex0$i
+	mkdir -p ex0$i
 	cd ex0$i
 	mkdir src
-	mkdir include
 	cd src
 	touch main.cpp
 	cd ..
+	mkdir include
 	cp -n ../../Makefile ./Makefile
 	cd ..
 done
