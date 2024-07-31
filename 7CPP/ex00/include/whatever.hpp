5c919f2b3abbd334ba9092bbbf518f8714df3da5
Julie Blaye <jblaye@bess-f4r5s10.clusters.42paris.fr>
2024-07-31 19:12:45 +0200

    preliminary work on templates

---
+++ 7CPP/ex00/include/whatever.hpp
@@ -0,0 +1,18 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>T max(T x, T y) {
+	return (x > y) ? x : y;
+}
+
+template <typename T>T min(T x, T y) {
+	return (x < y) ? x : y;
+}
+
+template <typename T>void swap(T x, T y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+
+#endif /* WHATEVER_HPP */
